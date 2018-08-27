16af7d5854d492e0b20773708b71f9ed36ac6bf9
kmbruce1986 <kmbruce1986@gmail.com>
2018-08-27 18:12:54 +0100

    adds bindEvents to result view file

--- src/views/result_view.js
+++ src/views/result_view.js
@@ -1,7 +1,16 @@
+const PubSub = require('../helpers/pub_sub.js')
+
 const ResultView = function () {
 
 };
 
+
+ResultView.prototype.bindEvents = function () {
+  PubSub.subscribe('WordCounter:result', (event) => {
+    const result = event.detail;
+    this.updateView(result);
+  })
+}
 
 ResultView.prototype.updateView = function () {
   const ResultElement = document.querySelector('#result');
