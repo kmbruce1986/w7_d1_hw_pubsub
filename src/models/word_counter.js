1aebe24896e4f733f9a80f4961602b106eda68a9
kmbruce1986 <kmbruce1986@gmail.com>
2018-08-27 18:10:17 +0100

    adds bindEvents to word counter

--- src/models/word_counter.js
+++ src/models/word_counter.js
@@ -2,6 +2,14 @@ const WordCounter = function () {
 
 };
 
+WordCounter.prototype.bindEvents = function () {
+  PubSub.subscribe('InputView:string-inputted', (event) => {
+    const inputtedString = event.detail;
+    const result = this.countWords(inputtedString);
+    PubSub.publish('WordCounter:result', result);
+  })
+}
+
 
 WordCounter.prototype.countWords = function (string) {
   result = string.split(" ").length
