3051766775c4f67b132223cbbc01d424319641de
kmbruce1986 <kmbruce1986@gmail.com>
2018-08-27 18:07:19 +0100

    adds pubsub publish to input view

--- src/views/input_view.js
+++ src/views/input_view.js
@@ -1,3 +1,5 @@
+const PubSub = require('../helpers/pub_sub.js')
+
 const InputView = function () {
 
 }
@@ -6,7 +8,7 @@ InputView.prototype.bindEvents() = function () {
   const input = document.querySelector('#string');
   input.addEventListener('input', event) => {
     const inputtedString = event.target.value;
-
+    PubSub.publish('InputView:string-inputted', inputtedString)
   }
 }
 
